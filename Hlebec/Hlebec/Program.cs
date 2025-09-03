5685e3c9a1d489e8d25063133c89d2667325488f
Kasey <koitvi4@gmail.com>
2025-09-04 00:01:00 +0500

    Add Vs project base code

---
+++ Hlebec/Hlebec/Program.cs
@@ -0,0 +1,15 @@
+﻿using Silk.NET.OpenGL;
+using System.Drawing;
+using Silk.NET.Input;
+using Silk.NET.Windowing;
+using Silk.NET.Maths;
+
+var options = WindowOptions.Default;
+options.Size = new Vector2D<int>(800, 600);
+options.Title = "Моё первое окно на Silk.NET";
+
+var window = Window.Create(options);
+
+
+
+window.Run();
