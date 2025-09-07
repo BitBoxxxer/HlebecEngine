1568885d5cd6489ae93a11b242d0dd74e08f3199
Kasey <koitvi4@gmail.com>
2025-09-07 19:36:08 +0500

    Add WPF design code base

---
+++ HlebecDesign/HlebecDesign/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HlebecDesign
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
